a20eb5800640b890de06337502ccaf1ef2a5f993
Developer Izi <hyunseong3020@naver.com>
2021-08-31 20:23:40 +0900

    Bigkinds 모듈로 받은 값들을 파일로 저장하고 전 과정을 자동화 및 최적화

--- bigkindsmodule.py
+++ bigkindsmodule.py
@@ -74,22 +74,47 @@ def big_kinds_samsung(mode, page):
     input_box.send_keys("삼성전자")
     input_box.send_keys(Keys.RETURN)
 
-    driver.implicitly_wait(time_to_wait=1)
-    driver.find_elements_by_xpath("//span[@class='title-elipsis']")[0].click()
-    # driver.find_elements_by_xpath("//a[@class='page-next page-link']")[0].click()
+    i = 0
+    while i < 1000:
+        j = 0
+        while j < 10:
+            driver.implicitly_wait(time_to_wait=1)
+            driver.find_elements_by_xpath("//span[@class='title-elipsis']")[j].click()
 
-    # time.sleep(0.1)
-    WebDriverWait(driver, timeout=5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="news-detail-modal"]/div/div/div[1]')))
+            # time.sleep(0.1)
+            WebDriverWait(driver, timeout=5).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="news-detail-modal"]/div/div/div[1]')))
 
-    html = driver.page_source  # 페이지 긁어오기
+            html = driver.page_source  # 페이지 긁어오기
 
-    soup = BeautifulSoup(html, 'html.parser')
-    temp_text_head = soup.select('.news-view-head')[0].get_text()
-    temp_text_body = soup.select('.news-view-body')[0].get_text()
+            soup = BeautifulSoup(html, 'html.parser')
+            temp_text_head = str(soup.select('.news-view-head')[0].get_text()).split("기사원문")[0].replace("\n\n", "")
+            temp_text_body = str(soup.select('.news-view-body')[0].get_text()).split("					")[1].replace(
+                "\n", "")
 
-    print(temp_text_body)
+            f = open("txt/" + str(i) + "-" + str(j) + ".txt", 'w', encoding='utf-8')
+            f.write(temp_text_head)
+            f.write(temp_text_body)
+            f.close()
 
-    close_button = driver.find_elements_by_xpath('//*[@id="news-detail-modal"]/div/div/button')[0]
-    close_button.click()
+            print_big(str(i) + "쪽" + str(j) + "번째 항목을 파일로 저장하였습니다. 저장명: \"" + str(i) + "-" + str(j) + "\"\n")
+            print(temp_text_head)
+            print(temp_text_body)
+            print("-----------------------------------------")
 
-    time.sleep(1000)
+            close_button = driver.find_elements_by_xpath('//*[@id="news-detail-modal"]/div/div/button')[0]
+
+            try:
+                close_button.click()
+            finally:
+                j += 1
+
+        driver.find_elements_by_xpath("//a[@class='page-next page-link']")[0].click()
+
+        i += 1
+
+        time.sleep(3)  # selenium.common.exceptions.StaleElementReferenceException 오류 방지
+
+    print_big("1쪽부터 1000쪽까지 총 10000개의 뉴스를 출력했습니다. 프로그램을 종료합니다.")
+
+    driver.close()
