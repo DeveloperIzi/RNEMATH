8c06a8ffd695fb95996e5a1424d0778855bfc889
Developer Izi <hyunseong3020@naver.com>
2021-08-31 19:50:25 +0900

    BigKinds 모듈로 삼성 관련 자료들을 받아볼수 있게 됨

--- bigkindsmodule.py
+++ bigkindsmodule.py
@@ -1,10 +1,15 @@
 # 크롤링, 파싱
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 from bs4 import *  # BeautifulSoup
 
 # 가독성을 위한 프린트
+from selenium.webdriver.support.wait import WebDriverWait
+
 from pyrint import print_big
 
 # 시간 조정
@@ -64,18 +69,27 @@ def big_kinds_samsung(mode, page):
     driver.get(url=url)
 
     input_box = driver.find_element_by_id('total-search-key')
-
-    term_button = driver.find_element_by_link_text('기간').click()
-
-    term_all_button = driver.find_elements_by_xpath("//span[@class='radio-btn date-radio-btn']/input[@id='date1-1']")
-
-    print(term_all_button)
-
-    # term_button = driver.find_elements_by_xpath("//a[text()='기간']")
-
-    time.sleep(5)
-
+    # term_all_button = driver.find_elements_by_xpath("//span[@class='radio-btn date-radio-btn']/input[@id='date1-1']")[0]
+    # driver.execute_script("arguments[0].click();", term_all_button)
     input_box.send_keys("삼성전자")
-    # input_box.send_keys(Keys.RETURN)
+    input_box.send_keys(Keys.RETURN)
+
+    driver.implicitly_wait(time_to_wait=1)
+    driver.find_elements_by_xpath("//span[@class='title-elipsis']")[0].click()
+    # driver.find_elements_by_xpath("//a[@class='page-next page-link']")[0].click()
+
+    # time.sleep(0.1)
+    WebDriverWait(driver, timeout=5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="news-detail-modal"]/div/div/div[1]')))
+
+    html = driver.page_source  # 페이지 긁어오기
+
+    soup = BeautifulSoup(html, 'html.parser')
+    temp_text_head = soup.select('.news-view-head')[0].get_text()
+    temp_text_body = soup.select('.news-view-body')[0].get_text()
+
+    print(temp_text_body)
+
+    close_button = driver.find_elements_by_xpath('//*[@id="news-detail-modal"]/div/div/button')[0]
+    close_button.click()
 
     time.sleep(1000)
